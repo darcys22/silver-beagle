7406980a8cadae1f01a393734ffd38376afe8312
Sean Darcy <darcys22@gmail.com>
2015-06-24 12:04:43 +1000

    landing page - not working at all

--- app/app.js
+++ app/app.js
@@ -2,4 +2,15 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-]);
+    'ngRoute',
+    'myApp.landing'
+]).
+config(['$routeProvider', function($routeProvider) {
+  // Set default view of our app to the landing page
+
+
+  $routeProvider.otherwise({
+    redirectTo: '/landing'
+  });
+
+}]);
---
+++ app/landing/landing.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('myApp.landing', ['ngRoute'])
+
+// Declared route
+.config([$routeProvider', function($routeProvider) {
+  $routeProvider.when('/landing', {
+    templateUrl: 'landing/landing.html',
+    controller: 'LandingController'
+  });
+}])
+
+.controller('LandingController', [function() {
+
+}]);
