5196d5af1662efeb7f3bc1265516e6db1567107d
Sean Darcy <darcys22@gmail.com>
2015-06-27 21:48:51 +1000

    repeat works, create a structure with example data

--- app/hordemind/hordemind.js
+++ app/hordemind/hordemind.js
@@ -1,39 +1,15 @@
 (function () {
-  var injectParams = ['$scope', '$location', '$routeParams',
-      '$timeout', 'config', 'dataService', 'modalService'];
 
+  var injectParams = ['$scope'];
 
-//At this point the customeredit controller is just something to copy shit from
-  var CustomerEditController = function ($scope, $location, $routeParams,
-      $timeout, config, dataService, modalService) {
-        var vm = this,
-        customerId = ($routeParams.customerId) ? parseInt($routeParams.customerId) : 0,
-        timer,
-        onRouteChangeOff;
-        vm.customer = {};
-        vm.states = [];
-        vm.title = (customerId > 0) ? 'Edit' : 'Add';
-        vm.buttonText = (customerId > 0) ? 'Update' : 'Add';
-        vm.updateStatus = false;
-        vm.errorMessage = '';
-        vm.isStateSelected = function (customerStateId, stateId) {
-          return customerStateId === stateId;
-        };
-        vm.saveCustomer = function () {
-          if ($scope.editForm.$valid) {
-            if (!vm.customer.id) {
-              dataService.insertCustomer(vm.customer).then(processSuccess, processError);
-            }
-            else {
-              dataService.updateCustomer(vm.customer).then(processSuccess, processError);
-            }
-          }
-        };
-      };
-//Ends Here
+  var HordemindController = function($scope) {
 
-
-  var HordemindController = function($scope) {};
+    $scope.number = 20;
+    $scope.getNumber = function(num) {
+      return new Array(num);
+    };
+  
+  };
 
   HordemindController.$inject = injectParams;
 
@@ -50,3 +26,33 @@
   })
 ();
 
+
+
+//At this point the customeredit controller is just something to copy shit from
+  //var CustomerEditController = function ($scope, $location, $routeParams,
+      //$timeout, config, dataService, modalService) {
+        //var vm = this,
+        //customerId = ($routeParams.customerId) ? parseInt($routeParams.customerId) : 0,
+        //timer,
+        //onRouteChangeOff;
+        //vm.customer = {};
+        //vm.states = [];
+        //vm.title = (customerId > 0) ? 'Edit' : 'Add';
+        //vm.buttonText = (customerId > 0) ? 'Update' : 'Add';
+        //vm.updateStatus = false;
+        //vm.errorMessage = '';
+        //vm.isStateSelected = function (customerStateId, stateId) {
+          //return customerStateId === stateId;
+        //};
+        //vm.saveCustomer = function () {
+          //if ($scope.editForm.$valid) {
+            //if (!vm.customer.id) {
+              //dataService.insertCustomer(vm.customer).then(processSuccess, processError);
+            //}
+            //else {
+              //dataService.updateCustomer(vm.customer).then(processSuccess, processError);
+            //}
+          //}
+        //};
+      //};
+//Ends Here
