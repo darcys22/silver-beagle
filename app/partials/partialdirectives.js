7d235adc8307f6eadeaf08eb4dce4d5f31c1802c
Sean Darcy <darcys22@gmail.com>
2015-07-06 11:59:26 +1000

    coverview directive added

--- app/partials/partialdirectives.js
+++ app/partials/partialdirectives.js
@@ -1,15 +1,22 @@
 (function () {
 
   var viewSelecter = function () {
-
     return {
       restrict: 'E',
       scope: false,
       templateUrl: 'partials/viewselecter.html'
     }
+  };
 
+  var coverView = function () {
+    return {
+      restrict: 'E',
+      scope: false,
+      templateUrl: 'partials/coverview.html'
+    }
   };
 
   angular.module('myApp').directive('viewSelecter', viewSelecter);
+  angular.module('myApp').directive('coverView', coverView);
 }());
 
